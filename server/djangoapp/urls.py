e81200242f72f4a8b12fa9d627b8db3c2d2244c7
vsamson-git <vsamson.my@gmail.com>
2023-12-19 19:31:42 -0500

    added index and about section. Needs review

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -8,7 +8,6 @@ urlpatterns = [
     # route is a string contains a URL pattern
     # view refers to the view function
     # name the URL
-
     # path for about view
 
     # path for contact us view
@@ -20,6 +19,7 @@ urlpatterns = [
     # path for logout
 
     path(route='', view=views.get_dealerships, name='index'),
+    path(route='about', view=views.about, name='about'),
 
     # path for dealer reviews view
 
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -15,11 +15,12 @@ logger = logging.getLogger(__name__)
 
 
 # Create your views here.
-
+def get_dealerships(request):
+    return render(request, 'index.html')
 
 # Create an `about` view to render a static about page
-# def about(request):
-# ...
+def about(request):
+    return render(request, 'about.html')
 
 
 # Create a `contact` view to return a static contact page
